cafe428fbf0ed98492d11ce9be962afd43fbeff7
Tyler Aubin <60904118+taubin17@users.noreply.github.com>
2020-11-13 11:13:10 -0500

    Update development.ai.ts

--- development.ai.ts
+++ development.ai.ts
@@ -48,7 +48,7 @@ export type Coord = [number, number];
 */
 
 // do not include "export const developmentScript = " with your submission
-export const developmentScript = `
+`
 let turns = -1;
 let player_moves = [];
 let usable_tiles = [];
